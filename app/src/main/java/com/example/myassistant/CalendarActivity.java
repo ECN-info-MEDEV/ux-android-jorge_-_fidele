daeb49e5c53942463742980543d0809cf2d6cb76
JorgeStone <jqcc17@gmail.com>
2022-03-17 12:59:09 +0100

    calendar squelette

--- app/src/main/java/com/example/myassistant/CalendarActivity.java
+++ app/src/main/java/com/example/myassistant/CalendarActivity.java
@@ -3,6 +3,7 @@ package com.example.myassistant;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class CalendarActivity extends AppCompatActivity {
 
@@ -10,5 +11,11 @@ public class CalendarActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void previousMonthAction(View view) {
+    }
+
+    public void nextMonthAction(View view) {
     }
 }
