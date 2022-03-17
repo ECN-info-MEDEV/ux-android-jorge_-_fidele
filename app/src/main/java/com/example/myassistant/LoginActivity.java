b626942fe36973359d985405b798b11e9c6dc87e
JorgeStone <jqcc17@gmail.com>
2022-03-17 11:42:30 +0100

    Login Implementation

---
+++ app/src/main/java/com/example/myassistant/LoginActivity.java
@@ -0,0 +1,44 @@
+package com.example.myassistant;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private static final String LOG_TAG = LoginActivity.class.getSimpleName();
+    private EditText mPasswordView, mUserNameView;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        mUserNameView = findViewById(R.id.username);
+        mPasswordView = findViewById(R.id.password);
+    }
+
+    public void launchMainActivity(View view) {
+        Log.d(LOG_TAG, "Button clicked!");
+        String UserName = mUserNameView.getText().toString();
+        String Pwd = mPasswordView.getText().toString();
+
+        if(UserName.equalsIgnoreCase("Suraj") && Pwd.equals("Suraj@123")){
+            Intent MainIntent = new Intent(LoginActivity.this, CalendarActivity.class);
+            startActivity(MainIntent);
+            Toast.makeText(LoginActivity.this,"You are Sign in Successfuly.", Toast.LENGTH_LONG).show();
+        }else
+        {
+            Toast.makeText(LoginActivity.this,"Sorry,User Name or Password is incorrect.", Toast.LENGTH_LONG).show();
+        }
+
+    }
+}
