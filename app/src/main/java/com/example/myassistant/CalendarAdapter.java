ff780a53fb1845c061b6cce5737e477d2830a947
twfid <wend-pouire-fidele.tiemtore@eleves.ec-nantes.fr>
2022-03-24 12:17:10 +0100

    Adding of class CalendarAdapter Adding of class CalendarViewHolder

---
+++ app/src/main/java/com/example/myassistant/CalendarAdapter.java
@@ -0,0 +1,55 @@
+package com.example.myassistant;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+
+public class CalendarAdapter extends RecyclerView.Adapter<CalendarViewHolder> {
+
+    public final ArrayList<LocalDate> daysOfMonth;
+    private final OnItemListener onItemListener;
+    public CalendarAdapter(ArrayList<LocalDate> daysOfMonth, OnItemListener onItemListener) {
+        this.daysOfMonth = daysOfMonth;
+        this.onItemListener = onItemListener;
+    }
+
+    @NonNull
+    @Override
+    public CalendarViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+        LayoutInflater inflater=LayoutInflater.from(parent.getContext());
+        View view= inflater.inflate(R.layout.calendar_cell,parent,false);
+        ViewGroup.LayoutParams layoutParams=view.getLayoutParams();
+        layoutParams.height=(int) (parent.getHeight()*0.1666666);
+        return new CalendarViewHolder(view,onItemListener,daysOfMonth);
+
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull CalendarViewHolder holder, int position) {
+
+        final LocalDate date = daysOfMonth.get(position);
+
+        holder.dayOfMonth.setText(String.valueOf(date.getDayOfMonth()));
+
+
+        //holder.dayOfMonth.setText(daysOfMonth.get(position));
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return daysOfMonth.size();
+    }
+
+    public interface OnItemListener
+    {
+         void OnItemClick(int position,LocalDate date);
+    }
+}
